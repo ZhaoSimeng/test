08ea286d14007c057d97652d797286a6d67d4280
Zhao-Simeng <ffwxam@hotmail.com>
2018-11-02 17:25:10 +0800

    change font

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->label->setFont(QFont("Times", 20, QFont::Bold));
 }
 
 MainWindow::~MainWindow()
